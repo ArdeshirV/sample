b082f146a4f8215710fb7422f5ddb780f49f6004
Ardeshir <ArdeshirV@protonmail.com>
2019-03-04 23:00:42 +0330

    Add copyright comments

--- header.h
+++ header.h
@@ -1,3 +1,6 @@
+// sample - Sample C++ CLI Application
+// header.h - Includes all header files for this project
+// Copyright (c) 2019 ardeshirv@protonmail.com, Licensed Under GPLv3
 #include <iostream>
 
 using namespace std;
