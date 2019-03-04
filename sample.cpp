1f0bcb7cfabdf8e614e9ca8ff3a1c5e2206cb3ba
Ardeshir <ArdeshirV@protonmail.com>
2019-03-04 11:58:01 +0330

    Apply wrong change to test git

--- sample.cpp
+++ sample.cpp
@@ -6,6 +6,4 @@ int main() {
     cout << "sample - Sample C++ CLI Application, Version 1.0" << endl;
     cout << "Copyright 2019 ardeshirv@protonmail.com, Licensed Under GPLv3" << endl;
     cout << "Hello Git!" << endl;
-    return 0;
-}
 
