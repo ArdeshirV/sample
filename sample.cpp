1ae4f77343fdd397ef318a5246217b0089d53561
Ardeshir <ArdeshirV@protonmail.com>
2019-03-04 11:56:15 +0330

    add copyright to sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,4 +1,10 @@
+// sample.cpp - Main Entry Point of Sample App
+// sample - Sample C++ CLI Application, Version 1.0
+// Copyright 2019 ardeshirv@protonmail.com, Licensed Under GPLv3
+
 int main() {
+    cout << "sample - Sample C++ CLI Application, Version 1.0" << endl;
+    cout << "Copyright 2019 ardeshirv@protonmail.com, Licensed Under GPLv3" << endl;
     cout << "Hello Git!" << endl;
     return 0;
 }
